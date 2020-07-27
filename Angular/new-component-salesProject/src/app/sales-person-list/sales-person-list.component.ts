30aeaf0d1cfa2c136ca8bf2333bf8d737d039f8d
sylleryum <alisson_piuco@yahoo.com.br>
2020-07-27 12:40:18 -0300

    finished component-bootstrap

--- Angular/new-component-salesProject/src/app/sales-person-list/sales-person-list.component.ts
+++ Angular/new-component-salesProject/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,16 +3,16 @@ import {SalesPerson} from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
 
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("name1", "last1", "name@domain.com", 1000),
-    new SalesPerson("name2", "last2", "name@domain.com", 2000),
-    new SalesPerson("name3", "last3", "name@domain.com", 3000),
-    new SalesPerson("name4", "last4", "name@domain.com", 4000)
+    new SalesPerson("name1", "last1", "name1@domain.com", 1000),
+    new SalesPerson("name2", "last2", "name2@domain.com", 2000),
+    new SalesPerson("name3", "last3", "name3@domain.com", 3000),
+    new SalesPerson("name4", "last4", "name4@domain.com", 4000)
   ];
 
   constructor() { }
